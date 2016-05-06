251740c00a4c7e51ed2ae08c333cbbb9c64f68fc
aleksandarkasabov <kasabova@uni.coventry.ac.uk>
2016-05-06 10:44:30 +0000

    assignment

---
+++ assignment/js/stickynav.js
@@ -0,0 +1,20 @@
+var navigation, yPos, button;
+function yScroll(){
+  navigation=document.getElementById('navigation');
+  button=document.getElementById('button');
+  yPos=window.pageYOffset;
+  if(yPos>300){
+    navigation.style.position="fixed";
+    navigation.style.top="0px";
+    navigation.style.backgroundColor="rgba(33,33,33,0.5)";
+    
+    
+  }
+  else{
+    navigation.style.position="absolute";
+    navigation.style.top="50vh";
+    navigation.style.backgroundColor="rgba(0,0,0,0)";
+    button.style.color="rgba(0,0,0,1)";
+  }
+}
+window.addEventListener("scroll", yScroll);
