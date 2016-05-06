9b3a0568a5bee03a401afc9256282f40cdd206c4
aleksandarkasabov <kasabova@uni.coventry.ac.uk>
2016-05-06 11:45:24 +0000

    final

--- error.py
+++ error.py
@@ -1,7 +1,7 @@
 from flask import Flask, make_response, abort, redirect
 
 app = Flask(__name__);
-
+#adding route for the main webpage and linking to the webpage
 @app.route('/')
 def hello_world():
     return redirect('https://preview.c9users.io/aleksandarkasabov/final/assignment/index.html?_c9_id=livepreview4&_c9_host=https://ide.c9.io')
@@ -9,7 +9,8 @@ def hello_world():
 @app.route('/users/<username>')
 def hello_me(username):
     return 'Hello ' + username + '!'
-    
+
+#adding route for giving an error and linking to the error page    
 @app.route('/error')
 @app.route('/errors')
 def error():
